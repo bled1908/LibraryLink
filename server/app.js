ffbffc666f76dd33eef527e26bb43983381ac6df
bled1908 <bled19082005@gmail.com>
2025-06-20 00:04:02 +0530

    authorisation router and api testing in postman part 1

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors"; // Import the cors module for Cross-Origin Resource Sharing
 import { connectDB } from "./database/db.js"; // Import the connectDB function to connect to the database
 import { errorMiddleware } from "./middlewares/errorMiddlewares.js"; // Import the error middleware for handling errors
+import authRouter from "./routes/authRouter.js"; // Import the authentication routes
 
 export const app = express(); // Create an instance of express and export it
 
@@ -17,11 +18,16 @@ app.use(cors({
     origin: [process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
     credentials: true // Allow credentials (cookies, authorization headers, etc.) to be sent
-})); // Middleware to enable CORS for the specified frontend URL
+})
+); // Middleware to enable CORS for the specified frontend URL
 app.use(cookieParser()); // Middleware to parse cookies from the request
 app.use(express.json()); // Middleware to parse JSON request bodies
 app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded request bodies
 
+app.use("/api/v1/auth", authRouter); // Use the authentication routes for requests to /api/v1/auth
+// http://localhost:4000/api/v1/auth/ - This is the base URL for authentication routes
+// Connect to the database
 connectDB();
+
 
 app.use(errorMiddleware); // Use the error middleware to handle errors globally
