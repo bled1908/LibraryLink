e24ea5ff0666920f47f82b3772a7508626a06ac6
bled1908 <bled19082005@gmail.com>
2025-06-20 22:24:41 +0530

    login and logout

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -90,3 +90,37 @@ export const verifyOTP = catchAsyncErrors(async (req, res, next) => {
         return next(new ErrorHandler("Internal server error.", 500));
     }
 });
+
+export const login = catchAsyncErrors(async (req, res, next) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return next(new ErrorHandler("Please enter all fields.", 400));
+    }
+    const user = await User.findOne({ email, accountVerified: true }).select("+password");
+    if (!user) {
+        return next(new ErrorHandler("Invalid email or password.", 400));
+    }
+    const isPasswordMatched = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatched) {
+        return next(new ErrorHandler("Invalid email or password.", 400));
+    }
+    sendToken(user, 200, "Login successful.", res);
+});
+
+export const logout = catchAsyncErrors(async (req, res, next) => {
+    res.status(200).cookie("token", "", {
+        expires: new Date(Date.now()),
+        httpOnly: true, 
+    }).json({
+        success: true,
+        message: "Logged out successfully."
+    });
+});
+
+export const getUser = catchAsyncErrors(async (req, res, next) => {
+    const user = req.user;
+    res.status(200).json({
+        success: true,
+        user,
+    });
+});
